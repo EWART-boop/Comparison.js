11b9bef1736f3766b7735f8966161b6d8226c068
EWART-boop <johnewartdelacruz@gmail.com>
2024-09-10 17:06:08 +0800

    Comparison.js.

---
+++ comparison.js
@@ -0,0 +1,73 @@
+console.log("'GREATER THAN'");
+const firstFavNumb = 19;
+const secondFavnumb = 10;
+console.log(firstFavNumb > secondFavnumb);
+
+console.log("'LESS THAN'");
+const FirstFavNumb = 19;
+const SecondFavnumb = 10;
+console.log(FirstFavNumb < SecondFavnumb);
+ 
+console.log("'GREATER THAN OR EQUAL TO'");
+const FirstFavNumB = 19;
+const SecondFavnumB = 10;
+console.log(FirstFavNumB >= SecondFavnumB);
+if(FirstFavNumB != SecondFavnumB){
+    console.log("FALSE");
+}
+
+console.log("'LESS THAN OR EQUAL TO'");
+const FirstFavnumB = 19;
+const SecondFavNumB = 10;
+console.log(FirstFavnumB <= SecondFavNumB);
+if(FirstFavnumB != SecondFavNumB){
+    console.log("FALSE");
+}
+
+console.log("'Strict equality'");
+const fisTfavnumb = 19;
+const sEcondfavnumb = 10;
+console.log(fisTfavnumb === sEcondfavnumb);
+
+console.log("'Loose Equality'");
+const fisTfavnumb1 = 19;
+const sEcondfavnumb2 = 10;
+console.log(fisTfavnumb1 == sEcondfavnumb2);
+
+console.log("'Strict Inequality'");
+const fisTfavnumb2 = 19;
+const sEcondfavnumb1 = 10;
+console.log(fisTfavnumb2 !== sEcondfavnumb1);
+
+
+console.log("'Loose Inequality'");
+const fisTfavnumb0 = 19;
+const sEcondfavnumb3 = 10;
+console.log(fisTfavnumb0 != sEcondfavnumb3);
+
+console.log("'AND'")
+const fiRstfavnum = 19;
+const SecoNdfavnum = 10;
+if(fiRstfavnum === SecoNdfavnum && fiRstfavnum === SecoNdfavnum ){
+     console.log('True');
+}
+else{
+    console.log('False');
+}
+
+console.log("'OR'")
+const fiRstfavnum5 = 19;
+const SecoNdfavnum4 = 10;
+if(fiRstfavnum5 < SecoNdfavnum4 || fiRstfavnum5 != SecoNdfavnum4 ){
+     console.log('False');
+}
+else{
+    console.log('True');
+}
+
+console.log("'NOT'")
+const fIrstfavnum8 = 19;
+const sEcondfavnum7 = 10;
+
+console.log(!fIrstfavnum8);
+console.log(!sEcondfavnum7);
